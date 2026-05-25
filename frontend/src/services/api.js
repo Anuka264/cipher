4b1b8d8e01a87989f771081d9788bdb61afb3cb0
Anudari Ganbaatar <anukganbaatar48@gmail.com>
2026-05-25 22:21:35 +0530

    edited

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API = axios.create({
     //baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001/api',
     baseURL: import.meta.env.PROD 
-        ? 'https://cipher-backend.onrender.com/api' 
+        ? 'https://cipher-backend-ivyu.onrender.com/api' 
         : 'http://localhost:5001/api',
 });
 
